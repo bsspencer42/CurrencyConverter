4c41093359fa029d6244b2f959c5a2c7ad1eeca5
bsspencer42 <bsspencer42@gmail.com>
2021-07-28 17:29:27 -0400

    Commit #2

--- src/sample/CurrencyConverter.java
+++ src/sample/CurrencyConverter.java
@@ -1,34 +1,30 @@
 package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import  javafx.event.EventHandler;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+//import javafx.scene.
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import  javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-
 public class CurrencyConverter extends Application {
+    // Instance variables
+    final static double EXCHANGE_RATE = 0.81;
 
-    public void start(Stage mainStage){
-        mainStage.setTitle("Hello World Program");
+    public void start(Stage mainStage) {
+        Label valueLb1 = new Label("Input Value : $");
+        Label poundsLb1 = new Label();
+        Text dollarsTxt = new Text();
+        Button exchangeBtn = new Button();
+        exchangeBtn.setOnAction();
 
-        Button btn = new Button();
-        btn.setText("Print Hello World");
-        btn.setOnAction(new CustomEventHandler());
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root,300,300);
-        mainStage.setScene(scene);
-        mainStage.show();
     }
 
-    private class CustomEventHandler implements EventHandler<ActionEvent>{
-        public void handle(ActionEvent event){
-            System.out.println("Hello World!");
-        }
-    }
 }
